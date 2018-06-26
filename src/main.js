d2ddc3962a5afe868cdfd8f5060095b88ac656ae
James Truong <james.gitting@gmail.com>
2018-06-26 13:03:01 -0700

    Defining routing of the components

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router';
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+
 import App from './App.vue'
+
+Vue.use(VueRouter);
+Vue.use(VueAxios, axios);
 
 Vue.config.productionTip = false
 
+const router = new VueRouter({mode: 'history'});
+
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router
 }).$mount('#app')
