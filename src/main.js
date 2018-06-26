dc75e0c7f1d8f9d1a68fcbc165e842b5aa961f17
James Truong <james.gitting@gmail.com>
2018-06-26 13:08:55 -0700

    Defining the routes array

--- src/main.js
+++ src/main.js
@@ -4,13 +4,34 @@ import VueAxios from 'vue-axios';
 import axios from 'axios';
 
 import App from './App.vue'
+import Create from './components/Create.vue';
+import Edit from './components/Edit.vue';
+import Index from './components/Index.vue';
 
 Vue.use(VueRouter);
 Vue.use(VueAxios, axios);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-const router = new VueRouter({mode: 'history'});
+const routes = [
+	{
+		name: 'Create',
+		path: '/create',
+		component: Create
+	},
+	{
+		name: 'Edit',
+		path: '/edit',
+		component: Edit
+	},
+	{
+		name: 'Index',
+		path: '/index',
+		component: Index
+	}
+]
+
+const router = new VueRouter({mode: 'history'}, routes: routes);
 
 new Vue({
   render: h => h(App),
